5d7bee6666d17501f8b7b911c2206e13073f9b49
Chris Ryskamp <ryskamp.chris@gmail.com>
2019-04-12 21:41:05 -0500

    first commit

--- models/Article.js
+++ models/Article.js
@@ -1,16 +1,13 @@
 var mongoose = require("mongoose");
-
-// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-
 var ArticleSchema = new Schema({
-  // `title` is required and of type String
+  
   title: {
     type: String,
     required: true
   },
-  // `link` is required and of type String
+  
   link: {
     type: String,
     required: true
@@ -22,8 +19,6 @@ var ArticleSchema = new Schema({
   }
 });
 
-// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the Article model
 module.exports = Article;
